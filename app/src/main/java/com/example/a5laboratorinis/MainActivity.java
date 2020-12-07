2987856b3c31a201dc20f1e1137f45d475fb40ca
Dominykas <dominykas.savickas@knf.stud.vu.lt>
2020-12-07 16:22:41 +0200

    Completed

--- app/src/main/java/com/example/a5laboratorinis/MainActivity.java
+++ app/src/main/java/com/example/a5laboratorinis/MainActivity.java
@@ -5,36 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvContent;
     TextView showCurrency;
     TextView inputCurrency;
+    ListView simpleListView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.tvContent = findViewById(R.id.tvContent);
-        this.showCurrency = findViewById(R.id.showCurrency);
         this.inputCurrency = findViewById(R.id.inputCurrency);
+        this.simpleListView = findViewById(R.id.simpleListView);
+        String[]currency={};
+        arrayList=new ArrayList<>(Arrays.asList(currency));
+        adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        simpleListView.setAdapter(adapter);
     }
 
     public void onBtnDownloadClick(View view) {
-        String input = inputCurrency.getText().toString();
-        showCurrency.setText(input);
+        final String input = inputCurrency.getText().toString();
         this.tvContent.setText("Loading...");
         new DataLoader(){
             @Override
             public void onPostExecute(String result)
             {
-                tvContent.setText(result);
+                arrayList.add(input +" - "+ result);
+                adapter.notifyDataSetChanged();
+                // tvContent.setText(input +" - "+ result);
+                tvContent.setText("Complete");
             }
         }.execute(input);
     }
